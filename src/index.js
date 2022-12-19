dc217ff7da974375d6be1bff5cc3467241b91fe2
Deeptirtha <tirtha1111222@gmail.com>
2022-12-19 15:01:48 +0530

    start

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const route = require("./router");
+const bodyParser= require("body-parser")
+const mongoose = require("mongoose");
+ const app = express();
+ app.use(express.json());
+
+ mongoose
+  .connect(
+    "mongodb+srv://DeeptirthaMukherjee:QYKI3k8QSKC4I7FZ@cluster1.khatgm1.mongodb.net/project5-db?retryWrites=true&w=majority",
+    { UseNewUrlParser: true }
+  )
+  .then(() => console.log("Mongo-Db is connected"))
+  .catch((err) => console.log(err.message))
+
+app.use("/", route);
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log("listening at " + (process.env.PORT || 3000))})
